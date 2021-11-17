21340716df102963936bcc142b823be2e1a90805
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2021-11-17 17:12:33 +0700

    add navigation bar

---
+++ components/NavigationBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="max-w-screen-sm border-b bg-gray-900 border-gray-700 w-full px-5 py-5 text-white">
+    <div class="flex flex-row justify-center">
+      <div class="mx-3">
+        <font-awesome-icon :icon="['fas', 'film']" class="text-normal text-white" />
+      </div>
+      <p>Netflix-Duplicate</p>
+    </div>
+  </div>
+</template>
