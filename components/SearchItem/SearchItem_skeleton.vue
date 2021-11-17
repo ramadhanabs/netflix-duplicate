06ffc3f21d0dcd71450fcc4b5eb60caf70edc3e2
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2021-11-17 17:13:49 +0700

    add skeleton

---
+++ components/SearchItem/SearchItem_skeleton.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-row my-5 px-5 gap-x-5">
+    <div class="bg-gray-600 w-20 h-28 rounded-xl animate-pulse"></div>
+    <div class="mt-3">
+      <div class="bg-gray-600 w-24 h-3 rounded-lg animate-pulse my-2"></div>
+      <div class="bg-gray-600 w-32 h-3 rounded-lg animate-pulse my-2"></div>
+      <div class="bg-gray-600 w-12 h-3 rounded-lg animate-pulse my-2"></div>
+    </div>
+  </div>
+</template>
---
+++ components/Slider/Slider_skeleton.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="inline-block px-2">
+      <div class="text-white my-5 mx-auto">
+        <div class="bg-gray-600 w-23 h-48 rounded-xl animate-pulse"></div>
+        <div class="bg-gray-600 w-32 h-3 rounded-lg animate-pulse my-2"></div>
+        <div class="bg-gray-600 w-32 h-3 rounded-lg animate-pulse my-2"></div>
+        <div class="bg-gray-600 w-12 h-3 rounded-lg animate-pulse my-2"></div>
+      </div>
+    </div>
+  </div>
+</template>
